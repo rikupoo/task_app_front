51dcd5cdff3a2ba4f8cda83c0b6d92d0a6f75a26
rikupoo <nemutir@gmail.com>
2022-09-19 09:26:49 +0900

    finished_welcome_page

--- components/welcome/welAppBar.vue
+++ components/welcome/welAppBar.vue
@@ -8,30 +8,64 @@
   >
   <app-logo
     @click.native="goTo('scroll-top')"/>
-    <v-toolbar-title>
+    <v-toolbar-title class="hidden-mobile-and-down">
       {{ appName }}
     </v-toolbar-title>
     <v-spacer />
 
-    <v-toolbar-items class="ml-2">
+    <v-toolbar-items class="ml-2 hidden-ipad-and-down">
       <v-btn
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
         text
+        :class="{ 'hidden-sm-and-down': (menu.title === 'about') }"
         @click="goTo(menu.title)"
       >
         {{ $t(`menus.${menu.title}`) }}
       </v-btn>
     </v-toolbar-items>
+    <signup-link />
+    <login-link />
+    <v-menu
+      bottom
+      nudge-left="110"
+      nudge-width="100"
+    >
+      <template v-slot:activator="{ on }">
+        <v-app-bar-nav-icon
+          class="hidden-ipad-and-up"
+          v-on="on"
+        />
+      </template>
+      <v-list
+        dense
+        class="hidden-ipad-and-up"
+      >
+        <v-list-item
+          v-for="(menu, i) in menus"
+          :key="`menu-list-${i}`"
+          exact
+          @click="goTo(menu.title)"
+        >
+          <v-list-item-title>
+            {{ $t(`menus.${menu.title}`) }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
 import appLogo from '~/components/ui/appLogo'
+import loginLink from '~/components/beforeLogin/loginLink'
+import signupLink from '~/components/beforeLogin/signupLink'
 
 export default {
   components: {
-    appLogo
+    appLogo,
+    signupLink,
+    loginLink
   },
   props: {
     menus: {
