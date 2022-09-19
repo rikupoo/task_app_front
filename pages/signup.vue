1e0a8379cb8fae17a791508f3ed2116ba46c2e4c
rikupoo <nemutir@gmail.com>
2022-09-19 10:51:24 +0900

    add_signup_form_components

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-    signup.vue
-  </div>
+  <bef-login-form-card>
+    <template
+      #form-card-content
+    >
+      <v-form
+      v-model="isValid">
+        <user-form-name />
+        <user-form-email />
+        <user-form-password />
+      </v-form>
+      <v-btn
+        :disabled="!isValid"
+        block
+        color="myblue"
+        class="white--text"
+      >
+        登録する
+      </v-btn>
+    </template>
+  </bef-login-form-card>
 </template>
 
 <script>
+import befLoginFormCard from '~/components/beforeLogin/befLoginFormCard'
+import userFormEmail from '~/components/user/userFormEmail'
+import userFormName from '~/components/user/userFormName'
+import userFormPassword from '~/components/user/userFormPassword'
+
 export default {
-  layout: 'beforeLogin'
+  components:{
+    userFormEmail,
+    userFormName,
+    userFormPassword,
+    befLoginFormCard
+  },
+  layout: 'beforeLogin',
+  data () {
+    return {
+      isValid: false
+    }
+  }
 }
 </script>
