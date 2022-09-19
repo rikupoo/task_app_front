0cfb2c70512e559932c7007ee1b29c54aae83f36
rikupoo <nemutir@gmail.com>
2022-09-19 14:44:31 +0900

    add_account_menu_pages_and_logout_page

---
+++ pages/logout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div />
+</template>
+<script>
+export default {
+  async middleware ({ store, redirect, from }) {
+    await store.dispatch('logout')
+    // 変更
+    if (from.name !== 'index') { return redirect('/') }
+    // return redirect('/') 削除
+  },
+  // 以下、追加
+  layout: 'logout',
+  beforeCreate () {
+    this.$router.replace('/')
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -18,5 +18,8 @@ export const mutations = {
 export const actions = {
   login ({ commit }) {
     commit('setLoggedIn', true)
+  },
+  logout ({ commit }) {
+    commit('setLoggedIn', false)
   }
 }
